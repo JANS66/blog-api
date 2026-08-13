b59770e87315c9266f1c0647befb56ad4d6cea5f
JANS66 <98973687+JANS66@users.noreply.github.com>
2026-08-13 18:53:31 +0300

    fix(db): resolve pg driver deprecation warning on concurrent queries

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
+// Create a pg Pool intance using databse URL
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Pass the pool into the PrismaPg adapter
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
+// Instantiate PrismaClient with the adapter
 export const prisma = new PrismaClient({ adapter });
