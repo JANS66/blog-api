8e92291983c74bf514ac017b0df0838680b64f15
JANS66 <98973687+JANS66@users.noreply.github.com>
2026-08-18 14:44:17 +0300

    fix(auth): move LoginPage.jsx to correct folder

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./schemas/LoginPage";
+import LoginPage from "./pages/LoginPage";
 
 export const router = createBrowserRouter([
   {
